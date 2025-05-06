c3908aa48012b14da4a2498c9220007464a8dded
Edwin Gathecha <72628855+gathecha254@users.noreply.github.com>
2025-05-06 13:38:55 +0300

    Update schemas.py

--- library-api/schemas.py
+++ library-api/schemas.py
@@ -1,1 +1,14 @@
+from pydantic import BaseModel
+from typing import Optional
 
+# Schema for creating a member
+class MemberCreate(BaseModel):
+    name: str
+    email: str
+
+# Schema for responding with member data
+class Member(MemberCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
